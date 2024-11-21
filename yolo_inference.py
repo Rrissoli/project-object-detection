62d4bd49f2436f9021961e4f35e5d46fa0fdaa1d
Raphael Christianini Rissoli <raphael.rissoli@detran.df>
2024-11-21 20:50:15 -0300

    Add YOLO inference script and update .gitignore

---
+++ yolo_inference.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+import cv2
+import numpy as np
+
+model = YOLO('yolov8x')
+
+results =  model.predict('Videos/corinthians-quase-gol.mp4', save=True)
+
+print(results[0])
+print("-----------------------------------------------")
+for box in results[0].boxes:
+    print(box)
