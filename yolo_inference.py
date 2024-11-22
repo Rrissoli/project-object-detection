09c9f0045ac616f63b9c5a93f912e489788eb424
Raphael Christianini Rissoli <raphael.rissoli@detran.df>
2024-11-22 10:10:04 -0300

    training

--- yolo_inference.py
+++ yolo_inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 import numpy as np
 
-model = YOLO('yolov8x')
+model = YOLO('models/best.pt')
 
 results =  model.predict('Videos/corinthians-quase-gol.mp4', save=True)
 
